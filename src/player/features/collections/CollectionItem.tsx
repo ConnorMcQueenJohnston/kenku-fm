1114f91c380dac84b4721677a2e2d6ab693d5e8d
ConnorMcQueenJohnston <ConnorMcQueenJohnston@users.noreply.github.com>
2024-12-10 00:45:42 +0000

    Unified all cards (simple)
    + Made sure all cards (called "Items") on the Home page has the same respective functionality.
    + TODO: This solution was to ensure functionality was consistent, but there could definitely be a refactor to greatly reuse components.

--- src/player/features/collections/CollectionItem.tsx
+++ src/player/features/collections/CollectionItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
@@ -7,85 +7,194 @@ import ShuffleIcon from "@mui/icons-material/ShuffleRounded";
 import IconButton from "@mui/material/IconButton";
 import Box from "@mui/material/Box";
 
-import { backgrounds, isBackground } from "../../backgrounds";
+import {backgrounds, isBackground} from "../../backgrounds";
 
-import { Collection } from "./collectionsSlice";
-import { useSelector } from "react-redux";
-import { RootState } from "../../app/store/store";
-import {selectSoundById, Sound} from "../sound/soundsSlice";
+import {Collection, removeCollection} from "./collectionsSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../app/store/store";
+import {selectAllSounds, selectSoundById, Sound} from "../sound/soundsSlice";
+import CardContent from "@mui/material/CardContent";
+import Stack from "@mui/material/Stack";
+import {Fade} from "@mui/material";
+import MoreVert from "@mui/icons-material/MoreVertRounded";
+import Tooltip from "@mui/material/Tooltip";
+import PauseIcon from "@mui/icons-material/PauseRounded";
+import PlayArrowIcon from "@mui/icons-material/PlayArrowRounded";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import {PlaylistSettings} from "../playlists/PlaylistSettings";
+import {removePlaylist} from "../playlists/playlistsSlice";
+import {CollectionSettings} from "./CollectionSettings";
 
 type CollectionItemProps = {
-  collection: Collection;
-  onSelect: (id: string) => void;
-  onPlay: (sound: Sound) => void;
+    collection: Collection;
+    onSelect: (id: string) => void;
+    onPlay: (sound: Sound) => void;
 };
 
 export function CollectionItem({
-  collection,
-  onSelect,
-  onPlay,
-}: CollectionItemProps) {
-  const collections = useSelector((state: RootState) => state.collections);
-  const image = isBackground(collection.background)
-    ? backgrounds[collection.background]
-    : collection.background;
+                                   collection,
+                                   onSelect,
+                                   onPlay,
+                               }: CollectionItemProps) {
 
-  function handleShuffle() {
-    let sounds = [...collection.sounds];
-    // Play a random sound from the collection
-    const soundId = sounds[Math.floor(Math.random() * sounds.length)];
-    const sound = useSelector(selectSoundById(soundId));
-    if (sound) {
-      onPlay(sound);
+    const collections = useSelector((state: RootState) => state.collections);
+    const allSounds = useSelector(selectAllSounds);
+
+    const image = isBackground(collection.background)
+        ? backgrounds[collection.background]
+        : collection.background;
+
+    const [settingsMenuOpen, setSettingsMenuOpen] = useState(false);
+    const [settingsMenuVisible, setSettingsMenuVisible] = useState(false);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const menuOpen = Boolean(anchorEl);
+
+    const dispatch = useDispatch();
+
+    function handleShuffle() {
+        let sounds = [...collection.sounds];
+        // Play a random sound from the collection
+        const soundId = sounds[Math.floor(Math.random() * sounds.length)];
+        const sound = allSounds.filter(x => x.id == soundId)[0];
+        if (sound) {
+            onPlay(sound);
+        }
     }
-  }
 
-  return (
-    <Card sx={{ position: "relative" }}>
-      <CardActionArea onClick={() => onSelect(collection.id)}>
-        <CardMedia
-          component="img"
-          height="200px"
-          image={image}
-          alt={"Background"}
-          sx={{ pointerEvents: "none" }}
-        />
-      </CardActionArea>
-      <Box
-        sx={{
-          backgroundImage:
-            "linear-gradient(0deg, #00000088 30%, #ffffff44 100%)",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          pointerEvents: "none",
-        }}
-      />
-      <Box
-        sx={{
-          padding: 2,
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          position: "absolute",
-          bottom: 0,
-          width: "100%",
-          pointerEvents: "none",
-        }}
-      >
-        <Typography variant="h5" component="div">
-          {collection.title}
-        </Typography>
-        <IconButton
-          aria-label="shuffle"
-          sx={{ pointerEvents: "all" }}
-          onClick={handleShuffle}
-        >
-          <ShuffleIcon sx={{ fontSize: "2rem" }} />
-        </IconButton>
-      </Box>
-    </Card>
-  );
+    function handleSettingsMenuVisible(isVisible: boolean) {
+        setSettingsMenuVisible(isVisible);
+    }
+
+    function handleMenuClick(event: React.MouseEvent<HTMLButtonElement>) {
+        setAnchorEl(event.currentTarget);
+    }
+
+    function handleMenuClose() {
+        setAnchorEl(null);
+    }
+
+    function handleEdit() {
+        setSettingsMenuOpen(true);
+        handleMenuClose();
+    }
+
+    function handleCopyID() {
+        navigator.clipboard.writeText(collection.id);
+        handleMenuClose();
+    }
+
+    function handleDelete() {
+        dispatch(removeCollection(collection.id));
+        handleMenuClose();
+    }
+
+    return (
+        <Card sx={{position: "relative"}}>
+            <CardActionArea onClick={() => onSelect(collection.id)} onMouseOver={() => handleSettingsMenuVisible(true)}
+                            onMouseOut={() => handleSettingsMenuVisible(false)}>
+                <CardMedia
+                    component="img"
+                    height="200px"
+                    image={image}
+                    alt={"Background"}
+                    sx={{pointerEvents: "none"}}
+                />
+            </CardActionArea>
+            <Box
+                sx={{
+                    backgroundImage:
+                        "linear-gradient(0deg, #00000088 30%, #ffffff44 100%)",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    pointerEvents: "none",
+                }}
+            />
+            <Box
+                sx={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    pointerEvents: "none",
+                }}
+            >
+                <CardContent sx={{
+                    height: "100%",
+                }}>
+                    <Stack sx={{
+                        height: "100%",
+                        display: "flex",
+                        direction: "column",
+                        alignItems: "space-between",
+                        justifyContent: "space-between"
+                    }}
+                    >
+
+                        <Box sx={{
+                            display: "flex",
+                            justifyContent: "flex-end",
+                        }}>
+                            <Fade in={settingsMenuVisible}>
+                                <IconButton sx={{
+                                    minWidth: "2rem",
+                                    flexShrink: 0,
+                                    pointerEvents: "auto"
+                                }} onClick={handleMenuClick} onMouseOver={() => handleSettingsMenuVisible(true)}>
+                                    <MoreVert/>
+                                </IconButton>
+                            </Fade>
+
+                        </Box>
+
+                        <Box sx={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center"
+                        }}>
+                            <Tooltip title={collection.title} placement="top">
+                                <Typography variant="h5" noWrap sx={{zIndex: 1, cursor: "pointer"}}>
+                                    {collection.title}
+                                </Typography>
+                            </Tooltip>
+                            <Tooltip title="Play a random sound from this collection.">
+                                <IconButton
+                                    aria-label="shuffle"
+                                    sx={{pointerEvents: "all"}}
+                                    onClick={handleShuffle}
+                                >
+                                    <ShuffleIcon sx={{fontSize: "2rem"}}/>
+                                </IconButton>
+                            </Tooltip>
+                        </Box>
+
+                    </Stack>
+                </CardContent>
+            </Box>
+
+            <Menu
+                id="collection-menu"
+                anchorEl={anchorEl}
+                open={menuOpen}
+                onClose={handleMenuClose}
+                MenuListProps={{
+                    "aria-labelledby": "more-button",
+                }}
+            >
+                <MenuItem onClick={handleEdit}>Edit</MenuItem>
+                <MenuItem onClick={handleCopyID}>Copy ID</MenuItem>
+                <MenuItem onClick={handleDelete}>Delete</MenuItem>
+            </Menu>
+            <CollectionSettings
+                collection={collection}
+                open={settingsMenuOpen}
+                onClose={() => setSettingsMenuOpen(false)}
+            />
+        </Card>
+    );
 }
+
--- src/player/features/playlists/PlaylistItem.tsx
+++ src/player/features/playlists/PlaylistItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
@@ -8,109 +8,210 @@ import PauseIcon from "@mui/icons-material/PauseRounded";
 import IconButton from "@mui/material/IconButton";
 import Box from "@mui/material/Box";
 
-import { backgrounds, isBackground } from "../../backgrounds";
+import {backgrounds, isBackground} from "../../backgrounds";
 
-import { Playlist} from "./playlistsSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../app/store/store";
-import { playPause, startQueue } from "./playlistPlaybackSlice";
+import {Playlist, removePlaylist} from "./playlistsSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../app/store/store";
+import {playPause, startQueue} from "./playlistPlaybackSlice";
 import {selectAllSounds, Sound} from "../sound/soundsSlice";
+import CardContent from "@mui/material/CardContent";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import {SceneSettings} from "../scene/SceneSettings";
+import {removeScene} from "../scene/scenesSlice";
+import {PlaylistSettings} from "./PlaylistSettings";
+import Stack from "@mui/material/Stack";
+import {Fade} from "@mui/material";
+import MoreVert from "@mui/icons-material/MoreVertRounded";
+import Tooltip from "@mui/material/Tooltip";
 
 type PlaylistItemProps = {
-  playlist: Playlist;
-  onSelect: (id: string) => void;
-  onPlay: (track: Sound) => void;
+    playlist: Playlist;
+    onSelect: (id: string) => void;
+    onPlay: (track: Sound) => void;
 };
 
 export function PlaylistItem({
-  playlist,
-  onSelect,
-  onPlay,
-}: PlaylistItemProps) {
-  const playing = useSelector(
-    (state: RootState) =>
-      state.playlistPlayback.playing &&
-      state.playlistPlayback.queue?.playlistId === playlist.id
-  );
-  const queue = useSelector((state: RootState) => state.playlistPlayback.queue);
-  const shuffle = useSelector(
-    (state: RootState) => state.playlistPlayback.shuffle
-  );
-  const allSounds = useSelector(selectAllSounds);
+                                 playlist,
+                                 onSelect,
+                                 onPlay,
+                             }: PlaylistItemProps) {
+    const playing = useSelector(
+        (state: RootState) =>
+            state.playlistPlayback.playing &&
+            state.playlistPlayback.queue?.playlistId === playlist.id
+    );
+    const queue = useSelector((state: RootState) => state.playlistPlayback.queue);
+    const shuffle = useSelector(
+        (state: RootState) => state.playlistPlayback.shuffle
+    );
+    const allSounds = useSelector(selectAllSounds);
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const image = isBackground(playlist.background)
-    ? backgrounds[playlist.background]
-    : playlist.background;
+    const image = isBackground(playlist.background)
+        ? backgrounds[playlist.background]
+        : playlist.background;
 
-  function handlePlay() {
-    if (queue?.playlistId === playlist.id) {
-      dispatch(playPause(!playing));
-    } else {
-      let tracks = [...playlist.tracks];
-      const trackIndex = shuffle
-        ? Math.floor(Math.random() * tracks.length)
-        : 0;
-      const trackId = tracks[trackIndex];
-      const track: Sound = allSounds.filter(x => x.id == trackId)[0];
-      if (track) {
-        dispatch(startQueue({ tracks, trackId, playlistId: playlist.id }));
-        onPlay(track);
-      }
+    const [settingsMenuOpen, setSettingsMenuOpen] = useState(false);
+    const [settingsMenuVisible, setSettingsMenuVisible] = useState(false);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const menuOpen = Boolean(anchorEl);
+
+    function handlePlay() {
+        if (queue?.playlistId === playlist.id) {
+            dispatch(playPause(!playing));
+        } else {
+            let tracks = [...playlist.tracks];
+            const trackIndex = shuffle
+                ? Math.floor(Math.random() * tracks.length)
+                : 0;
+            const trackId = tracks[trackIndex];
+            const track: Sound = allSounds.filter(x => x.id == trackId)[0];
+            if (track) {
+                dispatch(startQueue({tracks, trackId, playlistId: playlist.id}));
+                onPlay(track);
+            }
+        }
     }
-  }
 
-  return (
-    <Card sx={{ position: "relative" }}>
-      <CardActionArea onClick={() => onSelect(playlist.id)}>
-        <CardMedia
-          component="img"
-          height="200px"
-          image={image}
-          alt={"Background"}
-          sx={{ pointerEvents: "none" }}
-        />
-      </CardActionArea>
-      <Box
-        sx={{
-          backgroundImage:
-            "linear-gradient(0deg, #00000088 30%, #ffffff44 100%)",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          pointerEvents: "none",
-        }}
-      />
-      <Box
-        sx={{
-          padding: 2,
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          position: "absolute",
-          bottom: 0,
-          width: "100%",
-          pointerEvents: "none",
-        }}
-      >
-        <Typography variant="h5" component="div">
-          {playlist.title}
-        </Typography>
-        <IconButton
-          aria-label="play/pause"
-          sx={{ pointerEvents: "all" }}
-          onClick={handlePlay}
-        >
-          {playing ? (
-            <PauseIcon sx={{ fontSize: "2rem" }} />
-          ) : (
-            <PlayArrowIcon sx={{ fontSize: "2rem" }} />
-          )}
-        </IconButton>
-      </Box>
-    </Card>
-  );
+    function handleSettingsMenuVisible(isVisible: boolean) {
+        setSettingsMenuVisible(isVisible);
+    }
+
+    function handleMenuClick(event: React.MouseEvent<HTMLButtonElement>) {
+        setAnchorEl(event.currentTarget);
+    }
+
+    function handleMenuClose() {
+        setAnchorEl(null);
+    }
+
+    function handleEdit() {
+        setSettingsMenuOpen(true);
+        handleMenuClose();
+    }
+
+    function handleCopyID() {
+        navigator.clipboard.writeText(playlist.id);
+        handleMenuClose();
+    }
+
+    function handleDelete() {
+        dispatch(removePlaylist(playlist.id));
+        handleMenuClose();
+    }
+
+    return (
+        <Card sx={{position: "relative"}}>
+            <CardActionArea onClick={() => onSelect(playlist.id)} onMouseOver={() => handleSettingsMenuVisible(true)}
+                            onMouseOut={() => handleSettingsMenuVisible(false)}>
+                <CardMedia
+                    component="img"
+                    height="200px"
+                    image={image}
+                    alt={"Background"}
+                    sx={{pointerEvents: "none"}}
+                />
+            </CardActionArea>
+            <Box
+                sx={{
+                    backgroundImage:
+                        "linear-gradient(0deg, #00000088 30%, #ffffff44 100%)",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    pointerEvents: "none",
+                }}
+            />
+            <Box
+                sx={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    pointerEvents: "none",
+                }}
+            >
+                <CardContent sx={{
+                    height: "100%",
+                }}>
+                    <Stack sx={{
+                        height: "100%",
+                        display: "flex",
+                        direction: "column",
+                        alignItems: "space-between",
+                        justifyContent: "space-between"
+                    }}
+                    >
+
+                        <Box sx={{
+                            display: "flex",
+                            justifyContent: "flex-end",
+                        }}>
+                            <Fade in={settingsMenuVisible}>
+                                <IconButton sx={{
+                                    minWidth: "2rem",
+                                    flexShrink: 0,
+                                    pointerEvents: "auto"
+                                }} onClick={handleMenuClick} onMouseOver={() => handleSettingsMenuVisible(true)}>
+                                    <MoreVert/>
+                                </IconButton>
+                            </Fade>
+
+                        </Box>
+
+                        <Box sx={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center"
+                        }}>
+                            <Tooltip title={playlist.title} placement="top">
+                                <Typography variant="h5" noWrap sx={{zIndex: 1, cursor: "pointer"}}>
+                                    {playlist.title}
+                                </Typography>
+                            </Tooltip>
+                            <Tooltip title={"Play a random song from " + playlist.title} placement="top">
+                                <IconButton
+                                    aria-label="play/pause"
+                                    sx={{pointerEvents: "all"}}
+                                    onClick={handlePlay}
+                                >
+                                    {playing ? (
+                                        <PauseIcon sx={{fontSize: "2rem"}}/>
+                                    ) : (
+                                        <PlayArrowIcon sx={{fontSize: "2rem"}}/>
+                                    )}
+                                </IconButton>
+                            </Tooltip>
+                        </Box>
+
+                    </Stack>
+                </CardContent>
+            </Box>
+
+            <Menu
+                id="collection-menu"
+                anchorEl={anchorEl}
+                open={menuOpen}
+                onClose={handleMenuClose}
+                MenuListProps={{
+                    "aria-labelledby": "more-button",
+                }}
+            >
+                <MenuItem onClick={handleEdit}>Edit</MenuItem>
+                <MenuItem onClick={handleCopyID}>Copy ID</MenuItem>
+                <MenuItem onClick={handleDelete}>Delete</MenuItem>
+            </Menu>
+            <PlaylistSettings
+                playlist={playlist}
+                open={settingsMenuOpen}
+                onClose={() => setSettingsMenuOpen(false)}
+            />
+        </Card>
+    );
 }
